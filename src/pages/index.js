a233d4f8da6ac4ab038a743c8e0d2ef036c94dad
JoshuaBatty <joshpbatty@gmail.com>
2019-05-21 09:15:09 +0200

    pushing latest work

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,14 +87,33 @@ const IndexPage = ({data}, props) => {
                         <p>Nannou is a library that aims to make it easy for artists to express themselves with <b>simple, fast, reliable</b> code.</p>
                     </div>
                 </div>
-                <div>Box 3</div>
-                <div>Box 4</div>
-                <div>Box 5</div>
-                <div>Box 6</div>
-                <div>Box 7</div>
-                <div>Box 8</div>
-                <div>Box 9</div>
-                <div>Box 10</div>
+                
+                <div className={styles.left_row_text}>
+                    <h4>Batteries Included</h4>
+                    <p>One of the beauties of being a creative coder is that we have the potential to create works in a wide range of domains. Nannou aims to give equal priority to a full suite of creative I/O including graphics, multi-windowing, audio, lasers, lighting and more.</p>
+                </div>
+                <div className={styles.row3_image}>
+                    <div>Box 4</div>
+                </div>
+
+                <div className={styles.row4_image}>
+                    
+                </div>
+                <div className={styles.right_row_text}>
+                    <h4>A fast, modern language</h4>
+                    <p>When experimenting with the cutting edge in audiovisual processing high performance becomes a necessity. When performing live or creating long-running installations reliability becomes equally important. Rust ticks these boxes and more.</p>
+                </div>
+
+                <div className={styles.left_row_text}>
+                    <h4>Open Source & Liberally Licensed</h4>
+                    <p>Access to these technologies is often expensive and locked behind proprietary systems. We believe in democratising this space by open sourcing Nannou for everyone to use.</p>
+                </div>
+                <div className={styles.row5_image}>
+                    <div>Box 7</div>
+                </div> 
+                
+
+
                 <div>Box 11</div>
 
             </div>
