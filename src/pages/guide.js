8d48c4a892422b3ecfa3dc9cd59e81fc9519feeb
JoshuaBatty <joshpbatty@gmail.com>
2019-05-25 14:12:09 +0200

    removed mdbook

---
+++ src/pages/guide.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const GuidePage = () => {
+    return (
+        <Layout>
+            <SEO title="Guide" keywords={["Nannou", "Guide", "Tutorial", "Rust", "Audiovisual", "Examples"]}/>
+            <h1>Guide</h1>
+
+            <main>
+                
+                
+            </main>
+
+        </Layout>
+    )
+}
+
+export default GuidePage
